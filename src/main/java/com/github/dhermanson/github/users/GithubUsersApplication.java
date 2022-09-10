d2d35ef103c612e26cca6c0385eece2710be9684
Derick Hermanson <derick.hermanson@gmail.com>
2022-09-10 07:00:47 -0500

    initial commit from spring boot initializr zip

---
+++ src/main/java/com/github/dhermanson/github/users/GithubUsersApplication.java
@@ -0,0 +1,13 @@
+package com.github.dhermanson.github.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dhermanson/github/users/GithubUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dhermanson.github.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
